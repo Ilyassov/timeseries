766a916860e9cc785662d114bc70e7f9a717a4f3
Aliyar Ilyassov <ilyassovaliyar@yandex.ru>
2020-03-30 19:24:21 +0600

    filename changed

--- task3.py
+++ task3.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-X = pd.read_csv('usd_kzt_2019_2020.csv', encoding = 'UTF-8', delimiter = ',')
+X = pd.read_csv('usd_kzt.csv', encoding = 'UTF-8', delimiter = ',')
 X = X.drop(['USD_quant'], axis = 1)
 Date = X['Date'].values
 USD = X['USD'].values
